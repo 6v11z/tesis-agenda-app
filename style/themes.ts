f2837c4bd91e7afa3e8e003242e9d4ac980c0e6b
Luis Torres <luis.i6nacio@gmail.com>
2024-02-26 16:33:35 -0300

    Add new colors, components and radius

--- style/themes.ts
+++ style/themes.ts
@@ -1,27 +1,73 @@
+const lightColors = {
+  backgroundPrimary: "#ffffff",
+  backgroundSecondary: "#f6f6f6",
+  textPrimary: "#000000",
+  textSecondady: "#4a4a4a",
+  textTertiary: "#868686",
+  border: "#0000001f",
+} as const
+
+const darkColors = {
+  backgroundPrimary: "#161616",
+  backgroundSecondary: "#1c1c1c",
+  textPrimary: "#f6f6f6",
+  textSecondady: "#d6d6d6",
+  textTertiary: "#868686",
+  border: "#ffffff1f",
+} as const
+
 export const lightTheme = {
   colors: {
-    backgroundPrimary: "#ffffff",
-    backgroundSecondary: "#f6f6f6",
-    textPrimary: "#000000",
+    ...lightColors,
   },
-  margins: {
-    sm: 2,
-    md: 4,
-    lg: 8,
-    xl: 12,
+  components: {
+    container: {
+      flex: 1,
+      backgroundColor: lightColors.backgroundPrimary,
+      padding: 10,
+    },
+    textPrimary: {
+      color: lightColors.textPrimary,
+    },
+    textSecondary: {
+      color: lightColors.textSecondady,
+    },
+    textTertiary: {
+      color: lightColors.textTertiary,
+    },
+  },
+  radius: {
+    sm: 5,
+    md: 10,
+    lg: 15,
+    xl: 20,
   },
 } as const
 
 export const darkTheme = {
   colors: {
-    backgroundPrimary: "#161616",
-    backgroundSecondary: "#1c1c1c",
-    textPrimary: "#f6f6f6",
+    ...darkColors,
   },
-  margins: {
-    sm: 2,
-    md: 4,
-    lg: 8,
-    xl: 12,
+  components: {
+    container: {
+      flex: 1,
+      backgroundColor: darkColors.backgroundPrimary,
+      padding: 10,
+    },
+    textPrimary: {
+      color: darkColors.textPrimary,
+    },
+    textSecondary: {
+      color: darkColors.textSecondady,
+    },
+    textTertiary: {
+      color: darkColors.textTertiary,
+    },
+  },
+  radius: {
+    sm: 5,
+    md: 10,
+    lg: 15,
+    xl: 20,
   },
 } as const
